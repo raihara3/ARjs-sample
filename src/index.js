04d5e6e2b32257d4e77f50f7deb8331019a2bd62
raihara3 <raihara3@gmail.com>
2021-05-15 14:26:39 +0900

    :sparkles: update camera animation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const lobbyPos = document.getElementById('lobby').getAttribute('position')
+const room1Pos = document.getElementById('room1').getAttribute('position')
+
 AFRAME.registerComponent('door-on-touch', {
   schema: {
     room: {default: ''}
@@ -6,13 +9,18 @@ AFRAME.registerComponent('door-on-touch', {
   init: function () {
     const data = this.data
     this.el.addEventListener('click', function() {
-      console.log(data.room)
       const camera = document.getElementById('camera')
+      const animation = camera.getAttribute('animation')
+
+      let toPos = '0 0 0'
+      if(data.room === 'room1') {
+        toPos = animation.to === `${lobbyPos.x} 0 ${lobbyPos.z}`
+          ? `${room1Pos.x} 0 ${room1Pos.z}`
+          : `${lobbyPos.x} 0 ${lobbyPos.z}`
+      }
+
       camera.setAttribute('animation', {
-        property: 'position',
-        easing: 'linear',
-        to: '10 0 0',
-        dur: 1000
+        to: toPos,
       })
     })
   }
